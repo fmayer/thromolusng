ae56f5c35a35839ee3a0dc8edbf544d3fd53e22a
Florian Mayer <flormayer@aim.com>
2009-12-02 23:31:16 +0100

    Add copyright headers.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: us-ascii -*-
 
-# asynchia - asynchronous networking library
+# thromolusng - a game
 # Copyright (C) 2009 Florian Mayer
 
 # This program is free software: you can redistribute it and/or modify
